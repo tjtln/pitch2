4562a404843a5010db389a997f9455501d94f294
TJ Thielen <tthielen@sourceallies.com>
2025-07-29 22:18:27 -0500

    fixed lambda to websocket

--- pitch-backend/src/index.ts
+++ pitch-backend/src/index.ts
@@ -1,9 +1,30 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import {
+  APIGatewayProxyWebsocketEventV2,
+  APIGatewayProxyResultV2,
+} from 'aws-lambda';
 
-export const handler: APIGatewayProxyHandler = async (event) => {
+export const handler = async (
+  event: APIGatewayProxyWebsocketEventV2
+): Promise<APIGatewayProxyResultV2> => {
   console.log("Event:", event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda TS!" }),
-  };
+
+  switch (event.requestContext.eventType) {
+    case 'CONNECT':
+      console.log('Client connected:', event.requestContext.connectionId);
+      return { statusCode: 200 };
+
+    case 'DISCONNECT':
+      console.log('Client disconnected:', event.requestContext.connectionId);
+      return { statusCode: 200 };
+
+    case 'MESSAGE':
+      console.log('Message received:', event.body);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Echo: ${event.body}` }),
+      };
+
+    default:
+      return { statusCode: 400 };
+  }
 };

2e4d64dd6d50756c75eb960c62f68137de76c4be
TJ Thielen <tthielen@sourceallies.com>
2025-07-30 21:47:19 -0500

    replace cols instead of row

--- pitch-backend/src/routes/disconnect.ts
+++ pitch-backend/src/routes/disconnect.ts
@@ -2,18 +2,24 @@ import { APIGatewayProxyResultV2 } from "aws-lambda";
 import * as AWS from 'aws-sdk';
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-export default async function onDisconnect(event: any): Promise<APIGatewayProxyResultV2> {
-    const { connectionId } = event.requestContext;
 
-    await dynamoDB.put({
-        TableName: process.env.CONNECTIONS_TABLE!,
-        Item: {
-          PK: `CONNECTION#${connectionId}`,
-          connectionId,
-          status: 'disconnected',
-          endedAt: Date.now(),
-        },
-      }).promise();
-      
-      return {statusCode: 200}
-  };
+export default async function onDisconnect(event: any): Promise<APIGatewayProxyResultV2> {
+  const { connectionId } = event.requestContext;
+
+  await dynamoDB.update({
+    TableName: process.env.CONNECTIONS_TABLE!,
+    Key: {
+      PK: `CONNECTION#${connectionId}`,
+    },
+    UpdateExpression: 'SET #status = :status, endedAt = :endedAt',
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+    ExpressionAttributeValues: {
+      ':status': 'disconnected',
+      ':endedAt': Date.now(),
+    },
+  }).promise();
+
+  return { statusCode: 200 };
+}

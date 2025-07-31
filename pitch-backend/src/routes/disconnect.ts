629072b84270d3e54415063142449d2b651c9beb
TJ Thielen <tthielen@sourceallies.com>
2025-07-30 21:41:59 -0500

    added disconect

--- pitch-backend/src/routes/disconnect.ts
+++ pitch-backend/src/routes/disconnect.ts
@@ -1,5 +1,19 @@
 import { APIGatewayProxyResultV2 } from "aws-lambda";
+import * as AWS from 'aws-sdk';
 
-export default async function disconnect(event: any): Promise<APIGatewayProxyResultV2>{
-    return {statusCode: 200}
-}
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+export default async function onDisconnect(event: any): Promise<APIGatewayProxyResultV2> {
+    const { connectionId } = event.requestContext;
+
+    await dynamoDB.put({
+        TableName: process.env.CONNECTIONS_TABLE!,
+        Item: {
+          PK: `CONNECTION#${connectionId}`,
+          connectionId,
+          status: 'disconnected',
+          endedAt: Date.now(),
+        },
+      }).promise();
+      
+      return {statusCode: 200}
+  };

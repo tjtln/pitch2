3c5d5948ef1946d2db4cbe322ec252338eaa19a3
TJ Thielen <tthielen@sourceallies.com>
2025-07-30 20:29:18 -0500

    add more routes

--- pitch-backend/src/index.ts
+++ pitch-backend/src/index.ts
@@ -6,27 +6,27 @@ import  onDisconnect  from './routes/disconnect';
 export const handler = async (
   event: APIGatewayProxyWebsocketEventV2
 ): Promise<APIGatewayProxyResultV2> => {
-  // console.log("Event:", JSON.stringify(event));
+  console.log("Event:", JSON.stringify(event));
 
-  // const route = event.requestContext.routeKey;
-  // let body: any = {};
-  // try {
-  //   if (event.body) body = JSON.parse(event.body);
-  // } catch {
-  //   body = {};
-  // }
+  const route = event.requestContext.routeKey;
+  let body: any = {};
+  try {
+    if (event.body) body = JSON.parse(event.body);
+  } catch {
+    body = {};
+  }
 
-  // switch (route) {
-  //   case '$connect':
-  //     console.log("connecting");
-  //     return await onConnect(event);
-  //   case '$disconnect':
-  //     return await onDisconnect(event);
-  //   case 'dealHands':
-  //     return await onDealHands(event, body);
-  //   default:
-  //     console.warn(`No handler for route: ${route}`);
-  //     return { statusCode: 400, body: 'Unknown route' };
-  // }
+  switch (route) {
+    case '$connect':
+      console.log("connecting");
+      return await onConnect(event);
+    case '$disconnect':
+      return await onDisconnect(event);
+    case 'dealHands':
+      return await onDealHands(event, body);
+    default:
+      console.warn(`No handler for route: ${route}`);
+      return { statusCode: 400, body: 'Unknown route' };
+  }
   return {statusCode:200};
 };
